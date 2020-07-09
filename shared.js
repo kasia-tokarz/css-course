3810bedfca039b662478c43e6f403850a8021030
kasia-tokarz <k.kolinska@gmail.com>
2020-07-09 14:01:37 +0200

    added mobile nav

--- shared.js
+++ shared.js
@@ -2,8 +2,8 @@ var backdrop = document.querySelector(".backdrop");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var modal = document.querySelector(".modal");
 var buttonNo = document.querySelector(".modal__action--negative");
-//console.dir(selectPlanButtons);
-//console.dir(backdrop);
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 for (var i = 0; i<selectPlanButtons.length; i++){
     selectPlanButtons[i].addEventListener("click", function(){
@@ -13,10 +13,18 @@ for (var i = 0; i<selectPlanButtons.length; i++){
 }
 
 buttonNo.addEventListener("click", closeModal);
-backdrop.addEventListener("click", closeModal);
+
+backdrop.addEventListener("click", function(){
+    mobileNav.style.display = "none";
+    closeModal();
+});
 
 function closeModal(){
     modal.style.display="none";
     backdrop.style.display="none";
 };
 
+toggleButton.addEventListener("click", function(){
+    mobileNav.style.display = "block";
+    backdrop.style.display = "block";
+});
