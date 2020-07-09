165cea162cc4f9ffbbd0ffb1504266682892f5c7
kasia-tokarz <k.kolinska@gmail.com>
2020-07-09 14:51:29 +0200

    added javascript

--- shared.js
+++ shared.js
@@ -5,26 +5,40 @@ var buttonNo = document.querySelector(".modal__action--negative");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
 
+// console.dir(backdrop.style["background-image"]);
+
 for (var i = 0; i<selectPlanButtons.length; i++){
     selectPlanButtons[i].addEventListener("click", function(){
-        modal.style.display="block";
-        backdrop.style.display="block";
+        // modal.style.display="block";
+        // backdrop.style.display="block";
+        // modal.className = "open"; this will actually overwrite the complite class list!!
+        modal.classList.add("open");
+        backdrop.classList.add("open");
     });
 }
 
-buttonNo.addEventListener("click", closeModal);
+if (buttonNo){
+    buttonNo.addEventListener("click", closeModal);
+}
 
 backdrop.addEventListener("click", function(){
-    mobileNav.style.display = "none";
+    // mobileNav.style.display = "none";
+    mobileNav.classList.remove("open");
     closeModal();
 });
 
 function closeModal(){
-    modal.style.display="none";
-    backdrop.style.display="none";
+    if (modal){
+        modal.classList.remove("open");
+    }
+    // modal.style.display="none";
+    // backdrop.style.display="none";
+    backdrop.classList.remove("open");
 };
 
 toggleButton.addEventListener("click", function(){
-    mobileNav.style.display = "block";
-    backdrop.style.display = "block";
+    // mobileNav.style.display = "block";
+    // backdrop.style.display = "block";
+    mobileNav.classList.add("open");
+    backdrop.classList.add("open");
 });
